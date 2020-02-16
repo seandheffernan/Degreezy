4292addd2ed649e846150b1a5ea732c07ee0a6ee
Andrew Smith <smitha24@rpi.edu>
2020-02-15 21:48:58 -0500

    Added middleware for bodyParser

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 const port = 3000;
 
 app.get('/', (req, res) => res.send("Hello World"));
