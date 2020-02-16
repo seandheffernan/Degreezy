e9dbbb491d40b2d87efb640c075cf128902316fa
Andrew Smith <smitha24@rpi.edu>
2020-02-15 21:55:24 -0500

    Added Comments to the Server File

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
+// Dependencies
 import express from 'express';
 import bodyParser from 'body-parser';
+
+
 const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+// Server Start
 const port = 3000;
 
 app.get('/', (req, res) => res.send("Hello World"));
