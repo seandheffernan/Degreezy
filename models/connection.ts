3d7201999bae833d3bcb78b85825057b68525ea2
Andrew Smith <smitha24@rpi.edu>
2020-02-22 17:06:02 -0500

    Added new connection parameters

--- models/connection.ts
+++ models/connection.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-export function get_connection() {
-    mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {newUrlParser: true})
+export async function get_connection() {
+    mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
         .catch(error => console.log(error));
 }
-
