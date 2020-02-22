b003a3610063f108da8cbf8644c92fc97c38f496
Andrew Smith <smitha24@rpi.edu>
2020-02-22 00:33:26 -0500

    Moved connection into get_connection

--- models/connection.ts
+++ models/connection.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {newUrlParser: true})
-    .catch(error => console.log(error));
 
+export function get_connection() {
+    mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {newUrlParser: true})
+        .catch(error => console.log(error));
+}
 
