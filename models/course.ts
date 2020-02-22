fa742a916d0231b54139cade7cde6deddd9d7592
Andrew Smith <smitha24@rpi.edu>
2020-02-22 17:05:37 -0500

    Correct function to be asynchronus with a callback

--- models/course.ts
+++ models/course.ts
@@ -3,7 +3,7 @@ import {get_connection} from "./connection";
 
 
 export const course = mongoose.Schema({
-    course_code: Number,
+    course_code: String,
     course_number: Number,
     prerequisites: [
         {
@@ -20,9 +20,18 @@ export const course = mongoose.Schema({
 });
 
 
-export function get_course(code, number) {
-    let course_model = mongoose.model('Course', course);
-    course_model.findOne({course_code: code, course_number: number}, function(err, data) {
-        return data;
-    });
+export async function get_course(code, number, callback) {
+    await get_connection().then(() => {
+            let course_model = mongoose.model('Course', course);
+            course_model.findOne({course_code: code, course_number: number}, {}, function (data, err) {
+                callback(data, err);
+                mongoose.disconnect();
+            });
+        }
+    )
 }
+
+get_course('CSCI', 1000, function (data, err) {
+    console.log(err);
+    console.log(data);
+})
--- routes/course.ts
+++ routes/course.ts
@@ -5,7 +5,9 @@ import {get_course} from "../models/course";
 const course_router = express.Router();
 
 course_router.get('/', (req, res) => {
-    get_course(req.query.course_code, req.query.course_number)
+    get_course(req.query.course_code, req.query.course_number, function(err, data) {
+        res.send("Hello World");
+    });
 });
 
 export default course_router;
