31cc25abafacb38ede367659e1324cc9abfb2c3c
Andrew Smith <smitha24@rpi.edu>
2020-02-28 12:11:38 -0500

    Remove comment

--- models/connection.ts
+++ models/connection.ts
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
 
 export async function get_connection() {
-    // mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
-    //     .catch(error => console.log(error));
-
-    mongoose.connect("mongodb://localhost", {useNewUrlParser: true, useUnifiedTopology: true})
-        .catch(error => console.log(error));
+    mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    .catch(error => console.log(error));
 }
