8a2bef58585a32cf8ee3280a91445311446247f6
Andrew Son <sonj2@rpi.edu>
2020-03-10 15:50:10 -0400

    semesters to String, get/push/pull semester by ID
    This will require changes in the front end to work with the backend

--- server/models/semester.ts
+++ server/models/semester.ts
@@ -14,10 +14,13 @@ export const semester: Schema = new Schema({
 
 semester.index({'$**': 'text'});
 
-export function get_semester(_id, callback) {
-    let semester_model = mongoose.model('Semester', semester);
-    semester_model.findOne({name: _id}, function (data, err) {
-        callback(data, err);
+export async function get_semester(_id, callback) {
+    await get_connection().then(() => {
+        let semester_model = mongoose.model('Semester', semester);
+        semester_model.findById(_id, function (data, err) {
+            callback(data, err);
+            mongoose.disconnect();
+        });
     });
 }
 
@@ -32,26 +35,32 @@ export function insert_semester(semester_details, callback) {
     });
 }
 
-export function push_course(_id, course_name, callback) {
-    let semester_model = mongoose.model('Semester', semester);
-    var course = {course: course_name};
-    semester_model.findOneAndUpdate({name: _id}, {$push: {courses: course}}, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            callback(err);
-        }
+export async function push_course(_id, course_name, callback) {
+    await get_connection().then(() => {
+        let semester_model = mongoose.model('Semester', semester);
+        var course = { course: course_name };
+        semester_model.findByIdAndUpdate(_id, {$push: {courses: course}}, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                callback(err);
+            }
+            mongoose.disconnect();
+        });
     });
 }
 
-export function pull_course(_id, course_name, callback) {
-    let semester_model = mongoose.model('Semester', semester);
-    var course = {course: course_name};
-    semester_model.findOneAndUpdate({name: _id}, {$pull: {courses: course}}, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            callback(err);
-        }
-    });
+export async function pull_course(_id, course_name, callback) {
+    await get_connection().then(() => {
+        let semester_model = mongoose.model('Semester', semester);
+        var course = { course: course_name };
+        semester_model.findByIdAndUpdate(_id, {$pull: {courses: course}}, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                callback(err);
+            }
+            mongoose.disconnect();
+        });
+    })
 }
