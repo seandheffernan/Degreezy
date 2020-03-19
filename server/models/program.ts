9c02a136b04d8521db346eeae368812fdacb2dd7
Andrew Smith <smitha24@rpi.edu>
2020-03-19 16:16:19 -0500

    Updated Requirements

--- server/models/program.ts
+++ server/models/program.ts
@@ -1,22 +1,31 @@
 import mongoose from 'mongoose';
 import {course} from "./course";
-import { get_connection } from './connection';
+import {get_connection} from './connection';
 
 export const Programs = mongoose.Schema({
     name: String,
     ismajor: Boolean,
     electivecredits: Number,
-    classes_required: [
+    direct_requirements: [
         {
-            course: course
+            course: course // Potentially Object Ids
         }
-    ]
+    ],
+    block_requirements: [{
+        block_name: String,
+        courses: [
+            {
+                course: course
+            }
+        ],
+        numRequired: Number
+    }],
 });
 
 export async function insert_program(program_details, callback) {
     await get_connection().then(() => {
         let program_model = mongoose.model('Program', Programs);
-        program_model.create(program_details, function(err){
+        program_model.create(program_details, function (err) {
             if (err) {
                 console.log(err);
             } else {
