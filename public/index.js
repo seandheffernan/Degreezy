a6022158e1dbc97dec64fa9723c904651ccf1e26
Roham Hussain <roham.hussain@gmail.com>
2020-04-24 17:46:27 -0400

    BUG FIX: Proper course names added now

--- public/index.js
+++ public/index.js
@@ -144,71 +144,50 @@ app.controller('ctrl', function ($scope, $http) {
   // uses target of the drag (where it will be dropped) &
   // uses source of the drag (where the dragged element originated from)
   drake.on('drop', (el, target, source) => {
-    alert(el.id);
+    // alert(el.id);
     $scope.drop(source.id, target.id, el.id);
     // console.log(source.id);
     el.classList.add('ex-moved');
   });
-  $scope.drop = function(sourceID, semesterID, courseInfo){
-    console.log("INFO:" +courseInfo);
-    // alert(courseInfo);
+  $scope.drop = function(sourceID, semesterID, courseName){
 
-    var course_json = JSON.parse(courseInfo);
+    var to_delete = {
+      course: courseName,
+      _id: sourceID
+    };
 
-    if (sourceID != semesterID) {
-      // dragging from the queue
-      if (course_json.name) {
-        // alert(sourceID + " " + course_json.name);
-        // alert(semesterID + " " + course_json.name);
-        var to_delete = {
-          course: course_json.name,
-          _id: sourceID
-        };
+    var to_insert = {
+      course: courseName,
+      _id: semesterID
+    };
 
-        var to_insert = {
-          course: course_json.name,
-          _id: semesterID
-        };
-      }
-      // dragging from semesters
-      else {
-        // alert(semesterID);
-        // alert(sourceID + " " + course_json);
-        // alert(semesterID + " " + course_json);
-        var to_delete = {
-          course: course_json[0],
-          _id: sourceID
-        };
+    $http({
+        method: 'POST',
+        url: '/semesters/pull',
+        dataType: 'JSON',
+        data: to_delete
+      }).then(function successCallback(response) {
+          console.log("DELETE successful");
+      }, function errorCallback(response) {
+          console.log(response.data);
+    });
 
-        var to_insert = {
-          course: course_json[0],
-          _id: semesterID
-        };
-      }
+    $http({
+        method: 'PUT',
+        url: '/semesters/push',
+        dataType: 'JSON',
+        data: to_insert
+      }).then(function successCallback(response) {
+          console.log("PUT successful");
+      }, function errorCallback(response) {
+          console.log(response.data);
+    });
 
-      $http({
-          method: 'POST',
-          url: '/semesters/pull',
-          dataType: 'JSON',
-          data: to_delete
-        }).then(function successCallback(response) {
-            console.log("DELETE successful");
-        }, function errorCallback(response) {
-            console.log(response.data);
-      });
 
-      $http({
-          method: 'PUT',
-          url: '/semesters/push',
-          dataType: 'JSON',
-          data: to_insert
-        }).then(function successCallback(response) {
-            console.log("PUT successful");
-        }, function errorCallback(response) {
-            console.log(response.data);
-      });
-    }
+
   }
+
+
 });
 
 
