a524237bb4597255b1fd8b6eb1a1f3b0e33aaab9
seandheffernan <43757314+seandheffernan@users.noreply.github.com>
2020-04-24 17:40:02 -0400

    Added function to get all programs in the database.

--- server/routes/program.ts
+++ server/routes/program.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {insert_program, get_program, build_programs} from "../models/program"
+import {insert_program, get_program, build_programs, get_all_programs} from "../models/program"
 
 const program_router = express.Router();
 
@@ -26,7 +26,17 @@ program_router.get('/', (req, res) => {
     
 });
 
-program_router.post('/build', (req, res) => {
+program_router.get('/all', (res) => {
+    get_all_programs(function (err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+program_router.post('/build', (res) => {
     build_programs(function (err) {
         if (err) {
             res.send(err);
