ecd858457e21ded5a26df23be1b3308166c9a0ac
Roham Hussain <roham.hussain@gmail.com>
2020-04-24 16:39:45 -0400

    change to localhost

--- server/models/connection.ts
+++ server/models/connection.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 export async function get_connection() {
-    mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/websci_project?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
-        .catch(error => console.log(error));
-    // mongoose.connect("mongodb://localhost", {useNewUrlParser: true, useUnifiedTopology: true})
-    //     .catch(error => console.log(error));
+    // mongoose.connect("mongodb+srv://test:kuzmin@cluster0-djdw5.mongodb.net/websci_project?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    //    .catch(error => console.log(error));
+     mongoose.connect("mongodb://localhost", {useNewUrlParser: true, useUnifiedTopology: true})
+         .catch(error => console.log(error));
 }
