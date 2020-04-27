788a792cb1b2842555880963a05d4ac1302c0123
seandheffernan <43757314+seandheffernan@users.noreply.github.com>
2020-04-26 23:41:38 -0400

    Get request should not return json datatype.

--- public/index.js
+++ public/index.js
@@ -131,7 +131,6 @@ app.controller('ctrl', function ($scope, $http) {
       $http({
         method: 'GET',
         url: 'users/exportCSV?token=' + $scope.userObj.usertoken,
-        dataType: 'JSON',
       }).then(function successCallback(response) {
         console.log(response);
         var myWindow = window.open("", "CSV Data", "width=400, height = 200");
